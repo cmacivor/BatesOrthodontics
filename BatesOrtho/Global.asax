ab69e53cf9c63fd96b01ca3a2c257db65d54c4b5
cmacivor <cmacivor82@gmail.com>
2014-10-07 18:13:57 -0700

    Initial commit for entire solution

---
+++ BatesOrtho/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BatesOrtho.MvcApplication" Language="C#" %>
---
+++ BatesOrtho/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BatesOrtho.Startup))]
+namespace BatesOrtho
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
