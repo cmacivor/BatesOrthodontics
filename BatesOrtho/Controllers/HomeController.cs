550449fa15897c12555e11495c1862e0ca44c2ef
cmacivor <cmacivor82@gmail.com>
2014-10-16 14:27:28 -0700

    committing POCO's, database, EF, AppointmentRequest AJAX and basic HTML

--- BatesOrtho/Controllers/HomeController.cs
+++ BatesOrtho/Controllers/HomeController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BatesOrtho.Models;
 
 namespace BatesOrtho.Controllers
 {
     public class HomeController : Controller
     {
+        //private BatesOrthoEntities db = new BatesOrthoEntities();
+
         public ActionResult Index()
         {
             return View();
@@ -32,6 +35,17 @@ namespace BatesOrtho.Controllers
             ViewBag.Message = "appointment request";
 
             return View();
+        }
+
+        [HttpPost]
+        public JsonResult CreateAppointmentRequest(AppointmentRequest apptRequest)
+        {
+            using (var ctx = new BatesOrthoEntities())
+            {
+                ctx.AppointmentRequests.Add(apptRequest);
+                ctx.SaveChanges();
+            }
+            return Json("OK");
         }
     }
 }
---
+++ BatesOrtho/Scripts/CRUD.js
@@ -0,0 +1,56 @@
+﻿/// <reference path="jquery-1.10.2.intellisense.js" />
+/// <reference path="jquery-1.10.2.js" />
+
+//this file will hold all the CRUD AJAX calls and client side validation
+
+$(document).ready(function (){
+
+    $('#btnSaveAppointmentRequest').click(function () {
+        var firstName = $('#txtFirstName').val();
+        //alert(firstName);
+
+        var lastName = $('#txtLastName').val();
+        var dob = $('#txtDOB').val();
+        var respPartyFirstName = $('#txtRespPartyFirstName').val();
+        var respPartyLastName = $('#txtRespPartyLastName').val();
+        var newPatient = $('#ddlNewPatient').val();
+        var phone = $('#txtPhone').val();
+        var email = $('#txtEmail').val();
+        var modeOfContact = $('#ddlModeOfContact').val();
+        var convenientTimes = $('#txtConvenientTimes').val();
+        var howDidYouHear = $('#txtHowDidYouHear').val();
+        var dentistName = $('#txtGeneralDentistName').val();
+        var comments = $('#txtAdditionalComments').val();
+
+        $.ajax({
+            url: "/Home/CreateAppointmentRequest",
+            type: "POST",
+            contentType: 'application/json; charset=utf-8',
+            data: JSON.stringify({
+                apptRequest: {
+                    'FirstName': firstName,
+                    'LastName': lastName,
+                    'DOB': dob,
+                    'ResponsiblePartyFirstName': respPartyFirstName,
+                    'ResponsiblePartyLastName': respPartyLastName,
+                    'IsNewPatient': newPatient,
+                    'Phone': phone,
+                    'Email': email,
+                    'PreferredModeOfContact': modeOfContact,
+                    'ConvenientTimes': convenientTimes,
+                    'HowDidYouHear': howDidYouHear,
+                    'GeneralDentistName': dentistName,
+                    'AdditionalComments': comments
+                }
+            }),
+            success: function (data) {
+                alert(data);
+            },
+            error: function (data) {
+                alert(data);
+            }
+        });
+    });
+});
+
+
