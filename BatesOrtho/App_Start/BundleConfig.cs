9e786bb280fdbaf2496bb3a0221e4596c68ce918
cmacivor <cmacivor82@gmail.com>
2014-11-16 10:53:12 -0800

    fixed validation issue, added global.js references

--- BatesOrtho/App_Start/BundleConfig.cs
+++ BatesOrtho/App_Start/BundleConfig.cs
@@ -8,12 +8,16 @@ namespace BatesOrtho
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            //bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-1.10.2.js"));
 
-            //bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate.min.js"));
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        //"~/Scripts/jquery.validate.min.js"));
                         "~/Scripts/additional-methods.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                "~/Scripts/jquery.validate.min.js"));
+                       //"~/Scripts/additional-methods.min.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
